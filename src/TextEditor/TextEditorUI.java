445c3bc1035fdca539e05a74fce391b04960ef8e
GlassGhost <RoyPfund@gmail.com>
2011-02-02 20:51:59 -0600

    IDK

--- src/TextEditor/TextEditorUI.java
+++ src/TextEditor/TextEditorUI.java
@@ -23,87 +23,79 @@ Depending on user input may edit or save text files.
 package TextEditor;
 /********************************System Headers********************************/
 import java.io.*;
+import java.awt.event.*;
 import javax.swing.*;
 import java.util.regex.*;
 import java.lang.*;
 /*********************************GUI Creation*********************************/
 public class TextEditorUI extends JFrame
-implements javax.swing.event.ChangeListener, java.awt.event.MouseListener {
+implements ActionListener,
+		javax.swing.event.ChangeListener,
+		java.awt.event.MouseListener {
 JFileChooser chooser = new JFileChooser();
 JTextPane mainJTextPane = new JTextPane();
-//			JPanel noWrapPanel = new JPanel();
-//			JScrollPane mainJScrollPane = new JScrollPane(noWrapPanel);
-	JScrollPane mainJScrollPane = new JScrollPane(mainJTextPane);
-JMenuBar JMenuBar1 = new JMenuBar();
+JScrollPane mainJScrollPane = new JScrollPane(mainJTextPane);
+JMenuBar TEUIJMenuBar = new JMenuBar();
 	JMenu fileJMenu = new JMenu("File");
-		JMenuItem newJMenuItem = new JMenuItem("New");
-		JMenuItem openJMenuItem = new JMenuItem("Open");
-		JMenuItem saveJMenuItem = new JMenuItem("Save");
-		JMenuItem SaveAsJMenuItem = new JMenuItem("Save As");
-		JMenuItem quitJMenuItem = new JMenuItem("Quit");
+		JMenuItem newJMenuItem = new JMenuItem("New"),
+		openJMenuItem = new JMenuItem("Open"),
+		saveJMenuItem = new JMenuItem("Save"),
+		saveasJMenuItem = new JMenuItem("Save As"),
+		quitJMenuItem = new JMenuItem("Quit");
 	JMenu editJMenu = new JMenu("Edit");
-		JMenuItem undoJMenuItem = new JMenuItem("Undo");
-		JMenuItem redoJMenuItem = new JMenuItem("Redo");
-		JMenuItem cutJMenuItem = new JMenuItem("Cut");
-		JMenuItem copyJMenuItem = new JMenuItem("Copy");
-		JMenuItem pasteJMenuItem = new JMenuItem("Paste");
-		JMenuItem deleteJMenuItem = new JMenuItem("Delete");
-		JMenuItem selectJMenuItem = new JMenuItem("Select-All");
-		JMenuItem findJMenuItem = new JMenuItem("Find & Replace");
+		JMenuItem undoJMenuItem = new JMenuItem("Undo"),
+		redoJMenuItem = new JMenuItem("Redo"),
+		cutJMenuItem = new JMenuItem("Cut"),
+		copyJMenuItem = new JMenuItem("Copy"),
+		pasteJMenuItem = new JMenuItem("Paste"),
+		deleteJMenuItem = new JMenuItem("Delete"),
+		selectJMenuItem = new JMenuItem("Select-All"),
+		findAnReplaceJMenuItem = new JMenuItem("Find & Replace");
 	JMenu viewJMenu = new JMenu("View");
 		JCheckBoxMenuItem TextWrapCheckBox = new JCheckBoxMenuItem("Word-Wrap", true);
 	JMenu helpJMenu = new JMenu("Help");
 		JMenuItem aboutJMenuItem = new JMenuItem("About");
 
 public TextEditorUI() {
-	super("Text Editor");
+	this.setTitle("Text Editor");
 	this.setSize(640, 450);
 	this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	this.setVisible(true);
 //		noWrapPanel.add( mainJTextPane );
 	this.add(mainJScrollPane);
-	this.setJMenuBar(JMenuBar1);
-		JMenuBar1.add(fileJMenu);
-			newJMenuItem.addMouseListener(this);
-			fileJMenu.add(newJMenuItem);
-			openJMenuItem.addMouseListener(this);
-			fileJMenu.add(openJMenuItem);
-			openJMenuItem.addMouseListener(this);
-			fileJMenu.add(saveJMenuItem);
-			saveJMenuItem.addMouseListener(this);
-			fileJMenu.add(SaveAsJMenuItem);
-			SaveAsJMenuItem.addMouseListener(this);
+	this.setJMenuBar(TEUIJMenuBar);
+		TEUIJMenuBar.add(fileJMenu);
+			addmenuitemtomenu(fileJMenu, newJMenuItem);
+			addmenuitemtomenu(fileJMenu, openJMenuItem);
+			addmenuitemtomenu(fileJMenu, saveJMenuItem);
+			addmenuitemtomenu(fileJMenu, saveasJMenuItem);
 				fileJMenu.addSeparator();
-			fileJMenu.add(quitJMenuItem);
-			quitJMenuItem.addMouseListener(this);
-		JMenuBar1.add(editJMenu);
-			editJMenu.add(undoJMenuItem);
-			undoJMenuItem.addMouseListener(this);
-			editJMenu.add(redoJMenuItem);
-			redoJMenuItem.addMouseListener(this);
+			addmenuitemtomenu(fileJMenu, quitJMenuItem);
+		TEUIJMenuBar.add(editJMenu);
+			addmenuitemtomenu(editJMenu, undoJMenuItem);
+			addmenuitemtomenu(editJMenu, redoJMenuItem);
 				editJMenu.addSeparator();
-			editJMenu.add(cutJMenuItem);
-			cutJMenuItem.addMouseListener(this);
-			editJMenu.add(copyJMenuItem);
-			copyJMenuItem.addMouseListener(this);
-			editJMenu.add(pasteJMenuItem);
-			pasteJMenuItem.addMouseListener(this);
-			editJMenu.add(deleteJMenuItem);
-			deleteJMenuItem.addMouseListener(this);
+			addmenuitemtomenu(editJMenu, cutJMenuItem);
+			addmenuitemtomenu(editJMenu, copyJMenuItem);
+			addmenuitemtomenu(editJMenu, pasteJMenuItem);
+			addmenuitemtomenu(editJMenu, deleteJMenuItem);
 				editJMenu.addSeparator();
-			editJMenu.add(selectJMenuItem);
-			selectJMenuItem.addMouseListener(this);
-			editJMenu.add(findJMenuItem);
-			findJMenuItem.addMouseListener(this);
-		JMenuBar1.add(viewJMenu);
+			addmenuitemtomenu(editJMenu, selectJMenuItem);
+			addmenuitemtomenu(editJMenu, findAnReplaceJMenuItem);
+		TEUIJMenuBar.add(viewJMenu);
 			viewJMenu.add(TextWrapCheckBox);
 			TextWrapCheckBox.addChangeListener(this);
-		JMenuBar1.add(helpJMenu);
-			helpJMenu.add(aboutJMenuItem);
-			aboutJMenuItem.addMouseListener(this);
 			//TextWrapCheckBox.addpropertychangedlistener(this);
+		TEUIJMenuBar.add(helpJMenu);
+			addmenuitemtomenu(helpJMenu, aboutJMenuItem);
 }
 /********************************Event Handling********************************/
+public void actionPerformed( ActionEvent event ){
+//	String eventstr = event.getActionCommand();
+//	if ( eventstr.equals("New") ){
+//		//tabbedPane.add("Untitled", makeTextPanel() );
+//	}
+}
 public void stateChanged(javax.swing.event.ChangeEvent evt) {
 	if (evt.getSource() == TextWrapCheckBox) {
 		if (TextWrapCheckBox.getState() == true){
@@ -157,8 +149,9 @@ public void mouseReleased(java.awt.event.MouseEvent evt) {
 	}
 }
 
+/**********************************Functions***********************************/
 private void FileDialog(String filedialogtypeSTR) {
-//filedialog switch for desired filedialog, add to or change existing cases for your app.
+//filedialog switch for desired filedialog
 	if (chooser.showDialog(null, filedialogtypeSTR) == JFileChooser.APPROVE_OPTION) {
 	File chosenFile = chooser.getSelectedFile();
 		if (filedialogtypeSTR.contentEquals("Open")){//user picked open file
@@ -186,6 +179,17 @@ private void FileDialog(String filedialogtypeSTR) {
 			}
 		}
 	}
+}
+
+private void addmenuitemtomenu(JMenu parentmenu, JMenuItem MenuItemInstance){
+	parentmenu.add(MenuItemInstance);
+	MenuItemInstance.addMouseListener(this);
+}
+private void addmenuitemtomenu(JMenu parentmenu, JMenuItem MenuItemInstance, KeyStroke keymnemonic){
+	parentmenu.add(MenuItemInstance);
+	MenuItemInstance.addMouseListener(this);
+//	MenuItemInstance.addActionListener(this);
+	MenuItemInstance.setAccelerator(keymnemonic);
 }
 
 public static void main(String args[]) {
